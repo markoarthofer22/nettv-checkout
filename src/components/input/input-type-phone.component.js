aa25f05a94599c2f6ed1e152b79c36e685c87c5d
markoarthofer22 <67559410+markoarthofer22@users.noreply.github.com>
2020-10-01 14:21:56 +0200

    .

--- src/components/input/input-type-phone.component.js
+++ src/components/input/input-type-phone.component.js
@@ -79,7 +79,6 @@ const InputTypePhone = ({
 
     useEffect(() => {
         if (predefinedValue && predefinedDialValue) return;
-        console.log(copiedPhoneValue);
 
         checkForCountryPhone(countriesID);
     }, [countriesID, copiedPhoneValue]);
